de7daf38b9122d1ea593c04226e85251dd78367c
eugenio-perdomo <scnhp.0894@gmail.com>
2023-10-08 19:40:35 -0300

    Añado dropdowns depas

--- src/main/webapp/templates/footer.js
+++ src/main/webapp/templates/footer.js
@@ -8,6 +8,13 @@ document.write(`
             </a>
         </div>
         <ul class="nav col-md-4 justify-content-end list-unstyled d-flex" style="width:fit-content">
+            <li class"ms-3">
+                <a class="nav-link" href="../../pages/home/login.html">
+                    <button class="btn btn-outline-danger btn-sm">
+                        Cerrar Sesión
+                    </button>
+                </a>
+            </li>    
             <li class="ms-3">
                 <a class="text-muted" href="https://github.com/BraG29/turismo-uy-desktop">
                     <img src="../../assets/images/flight.png" width="48px">
