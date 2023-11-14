f7d4fd0ed2ed71201feab6a992f90a96d11157a7
ShodytheWolf <martin.antunez5@gmail.com>
2023-11-14 19:37:19 -0300

    me falto para

--- src/main/java/uy/turismo/webapp/servlets/ServletProfile.java
+++ src/main/java/uy/turismo/webapp/servlets/ServletProfile.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mysql.cj.Session;
+
 
 import uy.turismo.webapp.functions.Functions;
 import uy.turismo.webapp.ws.controller.DtProviderWS;
